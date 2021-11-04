e71600f194bdde1ab3c0987c0660cedb60a36281
Igossx <igoss1037@gmail.com>
2021-11-04 21:09:56 +0100

    Add Angular Cli and Cors policy

--- Startup.cs
+++ Startup.cs
@@ -36,6 +36,10 @@ namespace Cinema
             });
 
             services.AddControllers();
+
+            // Adding Cors Policy to Angular App
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cinema", Version = "v1" });
@@ -55,6 +59,8 @@ namespace Cinema
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
